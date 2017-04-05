61ef1145219cd8856fc6c249266a1528304270ed
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-04 21:47:57 -0700

    See #2. See #3. This might work.

--- bash/00/day-zero.bash
+++ bash/00/day-zero.bash
@@ -2,13 +2,14 @@
 
 read -t 5 inputString # get a line of input from stdin and save it to our variable
 
-# Write the second line of output
-if [[ $? -ne 0 ]]
-then
-  export inputString="There weren't no input."
-fi
-
 # Your first line of output goes here
 echo 'Hello, World.'
 
-echo $inputString
+
+# Write the second line of output
+if [[ $? -ne 0 ]]
+then
+  echo $inputString
+else
+  echo "There weren't no input."
+fi
