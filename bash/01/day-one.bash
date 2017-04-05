bad591a0dab0c62a74a931e6ac1f70d20c74591e
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 11:41:36 -0700

    That should do it for bash. See #1.

--- bash/01/day-one.bash
+++ bash/01/day-one.bash
@@ -11,8 +11,6 @@ then
   echo "There weren't no input."
 else
   echo "$int + $dec" | bc -l
-  export add_dec = $dec + $dec
-  export add_str = "$str is the best place to learn and practice coding!\n"
-
-  echo $add_int
+  echo "$dec + $dec" | bc -l
+  echo "$str is the best place to learn and practice coding!\n"
 fi 
