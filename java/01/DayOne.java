09cb3058964274a344b2eeb22128d1a2a8ebd54c
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 16:36:30 -0700

    Gonna bang out the Java solution. See #14.

---
+++ java/01/DayOne.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class DayOne {
+
+  public static void main(String[] args) {
+    int i = 4;
+    double d = 4.0;
+    String s = "Run's house ";
+
+    Scanner scan = new Scanner(System.in);
+
+    scan.close();
+
+  }
+
+}
