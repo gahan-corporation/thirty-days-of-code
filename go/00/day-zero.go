5a2801cd65d5381bc271ce595f5e9348882115e7
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-04 19:12:22 -0700

    Well, I'll have to research this a bit later. See #2. See #3.

--- go/00/day-zero.go
+++ go/00/day-zero.go
@@ -1,15 +1,11 @@
 package main
 
-import "bufio"
 import "fmt"
-import "os"
 
 func main() {
 
     //reader := bufio.NewReader(os.Stdin)
     //text, _ := reader.ReadString('\n')
-    text = "This is the text string."
 
     fmt.Println("Hello, World.")
-    fmt.Println(text)
 }
