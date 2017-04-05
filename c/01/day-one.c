976d91a42734fe234f8779dfd2220b70977db421
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 11:50:46 -0700

    For testing. See #1.

---
+++ c/01/day-one.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+  int i = 4;
+  double d = 8;
+  char s[] = "Run's house";
+
+  printf("%i\n",i);
+
+  printf("%d\n",d);
+
+  printf("%s\n",s);
+}
--- c/execute.bash
+++ c/execute.bash
@@ -1,3 +1,4 @@
 #1/bin/bash
 
 gcc c/00/day-zero.c
+gcc c/01/day-one.c
