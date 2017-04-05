98d700c86885bbb6e67c8bd6307cba8ac110e1e3
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-04 22:47:33 -0700

    This probably is still broken. See #2. See #3.

--- scala/00/day-zero.scala
+++ scala/00/day-zero.scala
@@ -1,10 +1,15 @@
+import scala.concurrent.duration._
+
 object Solution {
     def main(args: Array[String]) {
         // Print "Hello, World."
         println("Hello, World.")
 
         // Read a string variable
-        val s = scala.io.StdIn.readLine()
+        val s = {
+          val deadline = 5 seconds fromNow
+          scala.io.StdIn.readLine()
+        }
 
         // Print the value of the string variable
         println(s)
