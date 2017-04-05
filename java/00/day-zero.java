83d35eed65a15e6f1a0d8870bfaa43bbeae0e91c
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-04 19:00:01 -0700

    Java is picky about class names. See #2.

--- java/00/day-zero.java
+++ java/00/day-zero.java
@@ -5,7 +5,7 @@ import java.math.*;
 import java.util.regex.*;
 
 
-public class Solution {
+public class day-zero {
 	public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
 		Scanner scan = new Scanner(System.in); 
