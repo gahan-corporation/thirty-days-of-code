b5e0006b71d587ab4fb85c075af4234ef10baf7f
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-04 19:10:32 -0700

    Semaphore doesn't like reading from stdin. See #2.

--- go/00/day-zero.go
+++ go/00/day-zero.go
@@ -6,8 +6,9 @@ import "os"
 
 func main() {
 
-    reader := bufio.NewReader(os.Stdin)
-    text, _ := reader.ReadString('\n')
+    //reader := bufio.NewReader(os.Stdin)
+    //text, _ := reader.ReadString('\n')
+    text = "This is the text string."
 
     fmt.Println("Hello, World.")
     fmt.Println(text)
