4343775aba3e61a744868c76c68b033309bdf296
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-04 21:43:40 -0700

    That should do ruby. See #2. See #3.

--- ruby/00/day-zero.rb
+++ ruby/00/day-zero.rb
@@ -1,9 +1,13 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-#input_string = gets
-input_string = 'This is the input string.'
+require 'timeout'
 
 # Print a string literal saying "Hello, World." to stdout.
 puts 'Hello, World.'
 
-# TODO: Write a line of code here that prints the contents of input_string to stdout.
-puts input_string
+begin
+  Timeout::timeout(5) do
+    input_string = gets
+  end
+
+rescue Timeout::Error
+  puts "There weren't no input."
+end
