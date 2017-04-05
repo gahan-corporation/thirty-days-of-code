4c3be87fa73199f507dd1f60019ffffb5324a802
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 15:10:16 -0700

    Passed. See #6.

--- perl/01/day-one.pl
+++ perl/01/day-one.pl
@@ -1,9 +1,44 @@
 #!/usr/local/bin/perl
 
-$i = 4;
-$d = 4.0;
-$s = "Run's house "
+use strict;
+use warnings;
 
-$int = 0;
-$dec = 0.0;
-$str = "";
+my $i = 4;
+my $d = 4.0;
+my $s = 'HackerRank ';
+
+# Declare second integer, double, and String variables.
+my $int = 0;
+my $dec = 0.0;
+my $str = "";
+
+# Read and save an integer, double, and String to your variables.
+alarm(5);
+eval {
+  local $SIG{ALRM} = sub { die };
+  $int = <STDIN>;
+};
+
+alarm(5);
+eval {
+  local $SIG{ALRM} = sub { die };
+  $dec = <STDIN>;
+};
+
+alarm(5);
+eval {
+  local $SIG{ALRM} = sub { die };
+  $str = <STDIN>;
+};
+
+# Print the sum of both integer variables on a new line.
+$int = $i + $int;
+print $int."\n";
+
+# Print the sum of the double variables on a new line.
+$dec = $d + $dec;
+printf("%.1f\n", $dec);
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print $s.$str;
