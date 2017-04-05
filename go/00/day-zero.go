7ddd5d0d8e54d2585ab036da93ad0938ca79d9c9
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-04 21:54:44 -0700

    This supposedly will work for go. See #2. See #3.

--- go/00/day-zero.go
+++ go/00/day-zero.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "time"
+)
 
 func main() {
 
-    //reader := bufio.NewReader(os.Stdin)
-    //text, _ := reader.ReadString('\n')
+  reader := bufio.NewReader(os.Stdin)
+  //text, _ := reader.ReadString('\n')
 
-    fmt.Println("Hello, World.")
+  fmt.Println("Hello, World.")
+
+  for {
+    select {
+    case i := <-input:
+        fmt.Println(i)
+    case <-time.After(4000 * time.Millisecond):
+        fmt.Println("There weren't no input.")
+    }
+  }
 }
