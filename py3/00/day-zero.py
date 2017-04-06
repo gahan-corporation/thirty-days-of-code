2cd5e870800062d155e001f1ada276e465c41d0b
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 18:11:55 -0700

    Removed comments. See #7.

--- py3/00/day-zero.py
+++ py3/00/day-zero.py
@@ -3,13 +3,10 @@
 import select
 import sys
 
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
 input_string = select.select([sys.stdin], [], [], 5)
 
-# Print a string literal saying "Hello, World." to stdout.
 print('Hello, World.')
 
-# TODO: Write a line of code here that prints the contents of input_string to stdout.
 if len(input_string[0]) > 0:
     print(input_string[0])
 else:
