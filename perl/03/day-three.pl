3d1bd05aff793bd99686097adccc9a3c98d3ee7f
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-06 15:19:07 -0700

    Added integer input. See #19.

--- perl/03/day-three.pl
+++ perl/03/day-three.pl
@@ -1,1 +1,15 @@
 #!/usr/local/bin/perl
+
+use strict;
+use warnings;
+
+my $integer;
+
+alarm(1);
+eval {
+  local $SIG{ALRM} = sub { 
+    $integer = 20; 
+    die;
+  };
+  $integer = <STDIN>;
+};
