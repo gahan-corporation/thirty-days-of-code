d650425385c6da13ad70de9c9adaf6c6b63e800a
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 17:28:21 -0700

    Passed HackerRank. See #13.

--- ruby/01/day-one.rb
+++ ruby/01/day-one.rb
@@ -21,10 +21,18 @@ begin
 
   Timeout::timeout(1) do
     e = gets
+
+    f = d + e.to_f
+    
+    puts f
   end
 
   Timeout::timeout(1) do
     t = gets
+
+    u = s + t
+
+    puts u 
   end
 
 rescue Timeout::Error
