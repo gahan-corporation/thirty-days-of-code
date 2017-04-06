e921133078a005a075b3db9b6a905cd8fc17d83b
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-06 16:31:08 -0700

    This is a start. See #37.

--- py3/03/day-three.py
+++ py3/03/day-three.py
@@ -3,23 +3,9 @@
 import select
 import sys
 
-i = 4
-d = 4.0
-s = "Run's house "
-
-j = select.select([sys.stdin], [], [], 1)
-e = select.select([sys.stdin], [], [], 1)
-t = select.select([sys.stdin], [], [], 1)
+n = select.select([sys.stdin], [], [], 1)
 
 try:
-    k = i + int(j[0][0].readline())
-    print(k)
-
-    f = d + float(e[0][0].readline())
-    print(f)
-
-    s = "{0}{1}".format(s, t[0][0].readline())
-    print(s)
-except Exception as exc:
-    print(exc)
-
+    n = int(j[0][0].readline())
+except:
+    n = 20
