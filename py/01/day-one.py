c7e51e3cd19348dff2de4bbee018853dfcbc72e9
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 17:35:38 -0700

    Fixed missing imports. See #7.

--- py/01/day-one.py
+++ py/01/day-one.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+import select
+import sys
+
 i = 4
 d = 4.0
 s = "Run's house "
+
+j, e, t = select.select([sys.stdin], [sys.stdin], [sys.stdin], 1)
+
+k = i + j
+
+print(k)
+
+if len(input_string[0]) > 0:
+    print(input_string[0])
+else:
+    print("There weren't no input.")
