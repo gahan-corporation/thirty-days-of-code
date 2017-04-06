9255349b0edfcb5a1b1d3048100f070413572ebb
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 17:56:35 -0700

    It passes. See #7.

--- py/01/day-one.py
+++ py/01/day-one.py
@@ -21,9 +21,20 @@ c.start()
 t = input()
 c.cancel()
 
-
 try: 
     k = i + int(j)
     print(k)
+except Exception as exc:
+    print(exc)
+
+try:
+    e = d + float(f)
+    print(e)
+except Exception as exc:
+    print(exc)
+
+try:
+    u = "{0}{1}".format(s, t)
+    print(u)
 except Exception as exc:
     print(exc)
