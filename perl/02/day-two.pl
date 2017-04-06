11bf836d370c18d54c5753778e1eccfab505473f
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 18:57:19 -0700

    Updated gitignore. See #18.

--- perl/02/day-two.pl
+++ perl/02/day-two.pl
@@ -2,3 +2,42 @@
 
 use strict;
 use warnings;
+
+my $meal;
+my $tip;
+my $tax;
+my $total;
+
+alarm(1);
+eval {
+  local $SIG{ALRM} = sub { 
+    $meal = 100; 
+    die;
+  };
+  $meal = <STDIN>;
+};
+
+alarm(1);
+eval {
+  local $SIG{ALRM} = sub { 
+    $tip = 100; 
+    die;
+  };
+  $tip = <STDIN>;
+};
+
+alarm(1);
+eval {
+  local $SIG{ALRM} = sub { 
+    $tax = 9; 
+    die;
+  };
+  $tax = <STDIN>;
+};
+
+$tip = $tip / 100;
+$tax = $tax / 100;
+
+print $tip;
+print $tax;
+
