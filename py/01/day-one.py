170354ac2ed038839a7486e6bf4a2c75e8d6fbe8
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 17:52:00 -0700

    This works better. See #7.

--- py/01/day-one.py
+++ py/01/day-one.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 
-import select
-import sys
+from threading import Timer
 
 i = 4
 d = 4.0
 s = "Run's house "
 
-j = select.select([sys.stdin], [], [], 1)
-
-if len(j[0]) > 0:
-    j = j[0]
-
-    k = i + j
-
-    print(k)
-else:
-    print("There weren't no input.")
+a = Timer(1, print, ["That weren't no input."])
+a.start()
+j = input()
+a.cancel()
