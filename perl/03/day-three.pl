481562154050db8d6f9d2970d618f3a5f7d257ea
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-06 15:24:21 -0700

    This should pass. See #19.

--- perl/03/day-three.pl
+++ perl/03/day-three.pl
@@ -13,3 +13,13 @@ eval {
   };
   $integer = <STDIN>;
 };
+
+if ($integer % 2 != 0) {
+  print "Weird"
+} elsif ($integer % 2 == 0 && $integer > 1 && $integer < 6) {
+  print "Not Weird"
+} elsif ($integer % 2 == 0 && $integer > 5 && $integer < 21) {
+  print "Weird"
+} elsif ($integer % 2 == 0 && $integer > 20) {
+  print "Not Weird"
+}
