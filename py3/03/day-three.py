0e0817dc5a65fba6073da1c2e29ddbca21344c3b
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-06 16:32:56 -0700

    I believe that should do it. See #37.

--- py3/03/day-three.py
+++ py3/03/day-three.py
@@ -9,3 +9,12 @@ try:
     n = int(j[0][0].readline())
 except:
     n = 20
+
+if n % 2 == 1:
+    print("Weird")
+elif n % 2 == 0 and n > 1 and n < 6:
+    print("Not Weird")
+elif n % 2 == 0 and n > 5 and n < 21: 
+    print("Weird")
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
