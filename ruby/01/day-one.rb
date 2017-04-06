13499851f0c2830ef3d036bb3650597bbbf1b4bf
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 17:21:33 -0700

    Maybe the first one? See #13.

--- ruby/01/day-one.rb
+++ ruby/01/day-one.rb
@@ -1,5 +1,32 @@
 #!/usr/bin/env ruby
 
+require 'timeout'
+
 i = 4
 d = 4.0
 s = "Run's house "
+
+j = 0
+e = 0.0
+t = ""
+
+begin
+  Timeout::timeout(1) do
+    j = gets
+
+    k = i + j
+
+    puts k
+  end
+
+  Timeout::timeout(1) do
+    e = gets
+  end
+
+  Timeout::timeout(1) do
+    t = gets
+  end
+
+rescue Timeout::Error
+  puts "There weren't no input."
+end
