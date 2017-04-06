eb0b3bd0f17a7cf0527c7974412874752e16a0f1
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-06 15:43:44 -0700

    Added code for day four. See #31.

---
+++ java/04/DayFour.java
@@ -0,0 +1,27 @@
+public class DayFour {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		if (initialAge > 0) {
+            age = initialAge;
+        } else {
+            age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        }
+	}
+
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+        if (age < 13) {
+            System.out.println("You are young.");
+        } else if (age > 12 && age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
+	}
+
+	public void yearPasses() {
+        age++;
+	}
+}
