b1ce0884232c735f186a66632b852bb7afec8907
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 17:53:47 -0700

    Catching exceptions. See #7.

--- py/01/day-one.py
+++ py/01/day-one.py
@@ -10,3 +10,20 @@ a = Timer(1, print, ["That weren't no input."])
 a.start()
 j = input()
 a.cancel()
+
+b = Timer(1, print, ["That weren't no input."])
+b.start()
+e = input()
+b.cancel()
+
+c = Timer(1, print, ["That weren't no input."])
+c.start()
+t = input()
+c.cancel()
+
+
+try: 
+    k = i + int(j)
+    print(k)
+except Exception as exc:
+    print(exc)
