6506afc4e832e50119290f12b736fe186d5931ea
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-05 18:27:50 -0700

    Maybe this will pass Semaphore. See #7.

--- py3/01/day-one.py
+++ py3/01/day-one.py
@@ -1,40 +1,25 @@
 #!/usr/bin/env python3
 
-from threading import Timer
+import select
+import sys
 
 i = 4
 d = 4.0
 s = "Run's house "
 
-a = Timer(1, print, ["That weren't no input."])
-a.start()
-j = input()
-a.cancel()
+j = select.select([sys.stdin], [], [], 1)
+e = select.select([sys.stdin], [], [], 1)
+t = select.select([sys.stdin], [], [], 1)
 
-b = Timer(1, print, ["That weren't no input."])
-b.start()
-e = input()
-b.cancel()
-
-c = Timer(1, print, ["That weren't no input."])
-c.start()
-t = input()
-c.cancel()
-
-try: 
-    k = i + int(j)
+try:
+    k = i + int(j[0][0].readline())
     print(k)
+
+    f = d + float(e[0][0].readline())
+    print(f)
+
+    s = "{0}{1}".format(s, t[0][0].readline())
+    print(s)
 except Exception as exc:
     print(exc)
 
-try:
-    e = d + float(f)
-    print(e)
-except Exception as exc:
-    print(exc)
-
-try:
-    u = "{0}{1}".format(s, t)
-    print(u)
-except Exception as exc:
-    print(exc)
