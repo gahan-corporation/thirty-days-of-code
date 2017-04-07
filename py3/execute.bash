9cc528721d8b542d9c11a508e1cb57d51988a378
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-07 14:43:34 -0700

    Added file for testing. See #51.

--- py3/execute.bash
+++ py3/execute.bash
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+echo "Testing alexander-laughlin submissions.\n\n\n";
 python3 py3/00/day-zero.py
-echo "Test zero passed. \n";
+echo "\n\n\n Test zero passed. \n";
+
 python3 py3/01/day-one.py
-echo "Test one passed. \n";
+echo "\n\n\n Test one passed. \n";
+
 python3 py3/02/day-two.py
-echo "Test two passed. \n";
+echo "\n\n\n Test two passed. \n";
+
 python3 py3/03/day-three.py
-echo "Test three passed. \n";
+echo "\n\n\n Test three passed. \n";
+
 python3 py3/04/day-four.py
-echo "Test four passed. \n";
+echo "\n\n\n Test four passed. \n";
+
 python3 py3/05/day-five.py
-echo "Test five passed. \n";
+echo "\n\n\n Test five passed. \n";
+
 python3 py3/06/day-six.py
-echo "Test six passed. \n";
+echo "\n\n\n Test six passed. \n";
+
 python3 py3/07/day-seven.py
-echo "Test seven passed. \n";
+echo "\n\n\n Test seven passed. \n";
+
 python3 py3/08/day-eight.py
-echo "Test eight passed. \n";
+echo "\n\n\n Test eight passed. \n";
+
+python3 py3/09/day-nine.py
+echo "\n\n\n Test nine passed. \n";
+
