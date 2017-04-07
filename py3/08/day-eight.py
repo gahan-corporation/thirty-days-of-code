cf7cb20bbd7c006e83ced890cb707bb3a788feda
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-07 13:38:17 -0700

    Added file for execution. See #52.

---
+++ py3/08/day-eight.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import select
+import sys
+
+t = select.select([sys.stdin], [], [], 1)
+
+try:
+    t = int(t[0][0].readline())
+
+    u = select.select([sys.stdin], [], [], 1)
+    s = u[0][0].readline()
+        
+except Exception as e:
+    print(e)
+    t = 2
+    s = "0 9 8 1 3 7 3 9 1"
+
+sa = s.split(" ")
+sb = [i for i in reversed(sa)]
+sa = ' '.join(sb)
+print(sa)
