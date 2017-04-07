5633c3d53c32f4ea0a0602fbb483d3f03a724010
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-07 14:53:48 -0700

    printf might do the trick. See #51.

--- py3/execute.bash
+++ py3/execute.bash
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-echo "Testing alexander-laughlin submissions.";
-echo "";
+printf "Testing alexander-laughlin submissions.\n\n\n";
 python3 py3/00/day-zero.py
-echo "\n\n\n Test zero passed. \n";
+printf "\n\n\n Test zero passed. \n";
 
 python3 py3/01/day-one.py
-echo "\n\n\n Test one passed. \n";
+printf "\n\n\n Test one passed. \n";
 
 python3 py3/02/day-two.py
-echo "\n\n\n Test two passed. \n";
+printf "\n\n\n Test two passed. \n";
 
 python3 py3/03/day-three.py
-echo "\n\n\n Test three passed. \n";
+printf "\n\n\n Test three passed. \n";
 
 python3 py3/04/day-four.py
-echo "\n\n\n Test four passed. \n";
+printf "\n\n\n Test four passed. \n";
 
 python3 py3/05/day-five.py
-echo "\n\n\n Test five passed. \n";
+printf "\n\n\n Test five passed. \n";
 
 python3 py3/06/day-six.py
-echo "\n\n\n Test six passed. \n";
+printf "\n\n\n Test six passed. \n";
 
 python3 py3/07/day-seven.py
-echo "\n\n\n Test seven passed. \n";
+printf "\n\n\n Test seven passed. \n";
 
 python3 py3/08/day-eight.py
-echo "\n\n\n Test eight passed. \n";
+printf "\n\n\n Test eight passed. \n";
 
 python3 py3/09/day-nine.py
-echo "\n\n\n Test nine passed. \n";
+printf "\n\n\n Test nine passed. \n";
 
