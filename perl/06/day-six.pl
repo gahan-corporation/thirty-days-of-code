61bd3833303d38f9451b3d86ea9bbec79603781f
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-06 17:57:26 -0700

    This doesn't seem quite right. See #22.

--- perl/06/day-six.pl
+++ perl/06/day-six.pl
@@ -2,24 +2,41 @@
 
 use strict;
 use warnings;
+use Data::Dumper qw(Dumper);
 
-my $t;
+my $even;
 my $i;
 my $k;
+my $odd;
+my @s;
+my $t;
 
 alarm(1);
 eval {
   local $SIG{ALRM} = sub { 
-    my @s = qw(
-      I firmly believe that any man's finest hour, the greatest fulfillment of all that he holds dear, is that moment when he has worked his heart out in a good cause and lies exhausted on the field of battle - victorious.
-      There's no replacement for displacement.
-    ); 
+    $t = 2;
+
+    @s[0] = "I firmly believe that any man's finest hour, the greatest fulfillment of all that he holds dear, is that moment when he has worked his heart out in a good cause and lies exhausted on the field of battle - victorious.";
+    @s[1] = "There's no replacement for displacement.";
+
     die;
   };
-  $integer = <STDIN>;
+  $t = <STDIN>;
+
+  for ($i = 0; $i < $t; $i++) {
+    @s[$i] = <STDIN>; 
+  }
 };
 
-for ($i = 1; $i < 11; $i++) {
-  $k = $i * $integer;
-  print $integer . " x " . $i . " = " . $k . "\n";
+for ($i = 0; $i < $t; $i++) {
+  my @v = split //, $s[$i];
+
+  for (my $j = 0; $j < @v; $j++) {
+    if ($j % 2 == 0) {
+      $even = $even . $v[$j];
+    } else {
+      $odd = $odd . $v[$j];
+    }
+  }
+  printf("%s %s\n", $even, $odd);
 }
