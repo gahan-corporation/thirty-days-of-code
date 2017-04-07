1e29e84f9360e8a1f3eb81be46cf44d42cb1b20e
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-07 14:39:54 -0700

    Gonna calculate factorial. See #51.

--- py3/09/day-nine.py
+++ py3/09/day-nine.py
@@ -3,41 +3,12 @@
 import select
 import sys
 
-s = []
-u = None 
 t = select.select([sys.stdin], [], [], 1)
 
 try:
     t = int(t[0][0].readline())
 
-    u = select.select([sys.stdin], [], [], 1)
-    s = u[0][0].readlines()
 except Exception as e:
     print(e)
-    t = 5
-    s = [
-        "Billy 3107281113",
-        "Brad 8182229181",
-        "Johnny 2021849131",
-        "Angelina 4241982193",
-        "Jennifer 2918881111",
-        "Brad",
-        "Jennifer",
-        "Billy",
-        "Joseph",
-    ] 
+    t = 50
 
-phone_book = {}
-
-for si in s:
-    sa = si.split(" ")
-    if len(sa) > 1:
-        name=sa[0].strip()
-        number=sa[1].strip()
-        phone_book.update({name:number})
-    if len(sa) == 1:
-        search=sa[0].strip()
-        if phone_book.get(search) != None:
-            print("{0}={1}".format(search,phone_book.get(search)))
-        else:
-            print("Not found")
