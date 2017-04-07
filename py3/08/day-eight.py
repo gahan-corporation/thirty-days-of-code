cd54bb0cf03cd30cea04e72d0f0be7f1716cce04
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-07 13:47:07 -0700

    It's a start. See #52.

--- py3/08/day-eight.py
+++ py3/08/day-eight.py
@@ -3,20 +3,34 @@
 import select
 import sys
 
+s = []
+u = None 
 t = select.select([sys.stdin], [], [], 1)
 
 try:
     t = int(t[0][0].readline())
 
-    u = select.select([sys.stdin], [], [], 1)
-    s = u[0][0].readline()
+    while u != "":
+        u = select.select([sys.stdin], [], [], 1)
+        s.append(u[0][0].readline())
         
 except Exception as e:
     print(e)
-    t = 2
-    s = "0 9 8 1 3 7 3 9 1"
+    t = 5
+    s = [
+        "Billy Bob Thornton 3107281113",
+        "Brad Pitt 8182229181",
+        "Johnny Lee Miller 2021849131",
+        "Angelina Jolie 4241982193",
+        "Jennifer Aniston 2918881111",
+        "Brad Pitt",
+        "Jennifer Aniston",
+        "Billy Bob Thornton",
+    ] 
 
-sa = s.split(" ")
+for si in s:
+    sa = s.split(" ")
+    print(sa)
 sb = [i for i in reversed(sa)]
 sa = ' '.join(sb)
 print(sa)
