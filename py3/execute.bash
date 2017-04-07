356186186348da21241edaca8ba36e2163c6766f
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-07 13:40:23 -0700

    Added testing. See #52.

--- py3/execute.bash
+++ py3/execute.bash
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 python3 py3/00/day-zero.py
+echo "Test zero passed. \n";
 python3 py3/01/day-one.py
+echo "Test one passed. \n";
 python3 py3/02/day-two.py
+echo "Test two passed. \n";
 python3 py3/03/day-three.py
+echo "Test three passed. \n";
 python3 py3/04/day-four.py
+echo "Test four passed. \n";
 python3 py3/05/day-five.py
+echo "Test five passed. \n";
 python3 py3/06/day-six.py
+echo "Test six passed. \n";
 python3 py3/07/day-seven.py
+echo "Test seven passed. \n";
 python3 py3/08/day-eight.py
+echo "Test eight passed. \n";
