c336980fb4f6af4ef617fec9e59f25797545c5ed
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-08 00:17:08 -0700

    It passed HackerRank. See #46.

--- java/06/DaySix.java
+++ java/06/DaySix.java
@@ -11,9 +11,21 @@ public class DaySix {
         int n = in.nextInt();
         String[] s = new String[n];
 
+        in.nextLine();
         for (int i = 0; i < n; i++) {
+          String e = new String();
+          String o = new String();
           s[i] = in.nextLine(); 
-          System.out.println(s[i]);
+
+          for (int j = 0; j < s[i].length(); j++) {
+            char c = s[i].charAt(j);
+            if (j % 2 == 1) {
+              o = o + c;
+            } else {
+              e = e + c;
+            }
+          }
+          System.out.println(e + " " + o);
         }
     }
 }
