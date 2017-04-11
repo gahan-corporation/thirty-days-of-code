b72fef8e595cfb57aef50968e04abfd89687806a
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-04-10 20:13:19 -0700

    Added tests for Jess. See #58.

--- py3/execute.bash
+++ py3/execute.bash
@@ -3,31 +3,44 @@
 printf "Testing alexander-laughlin submissions.\n\n\n";
 python3 py3/alexander-laughlin/00/day-zero.py
 printf "\nTest zero passed.\n\n";
-
 python3 py3/alexander-laughlin/01/day-one.py
 printf "\nTest one passed. \n\n";
-
 python3 py3/alexander-laughlin/02/day-two.py
 printf "\nTest two passed. \n\n";
-
 python3 py3/alexander-laughlin/03/day-three.py
 printf "\nTest three passed. \n\n";
-
 python3 py3/alexander-laughlin/04/day-four.py
 printf "\nTest four passed. \n\n";
-
 python3 py3/alexander-laughlin/05/day-five.py
 printf "\nTest five passed. \n\n";
-
 python3 py3/alexander-laughlin/06/day-six.py
 printf "\nTest six passed. \n\n";
-
 python3 py3/alexander-laughlin/07/day-seven.py
 printf "\nTest seven passed. \n\n";
-
 python3 py3/alexander-laughlin/08/day-eight.py
 printf "\nTest eight passed. \n\n";
-
 python3 py3/alexander-laughlin/09/day-nine.py
+printf "\nTest nine passed. \n\n";
+
+printf "Testing jessica-laughlin submissions.\n\n\n";
+python3 py3/jessica-laughlin/00/day-zero.py
+printf "\nTest zero passed.\n\n";
+python3 py3/jessica-laughlin/01/day-one.py
+printf "\nTest one passed. \n\n";
+python3 py3/jessica-laughlin/02/day-two.py
+printf "\nTest two passed. \n\n";
+python3 py3/jessica-laughlin/03/day-three.py
+printf "\nTest three passed. \n\n";
+python3 py3/jessica-laughlin/04/day-four.py
+printf "\nTest four passed. \n\n";
+python3 py3/jessica-laughlin/05/day-five.py
+printf "\nTest five passed. \n\n";
+python3 py3/jessica-laughlin/06/day-six.py
+printf "\nTest six passed. \n\n";
+python3 py3/jessica-laughlin/07/day-seven.py
+printf "\nTest seven passed. \n\n";
+python3 py3/jessica-laughlin/08/day-eight.py
+printf "\nTest eight passed. \n\n";
+python3 py3/jessica-laughlin/09/day-nine.py
 printf "\nTest nine passed. \n\n";
 
