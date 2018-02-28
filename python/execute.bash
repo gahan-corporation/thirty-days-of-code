8136fb875f74ac458b91763867e438a66216920b
Xander Harris <duchess@gahan-corporation.com>
2018-02-28 07:28:13 -0800

    Update execute. See #60.

--- python/execute.bash
+++ python/execute.bash
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-printf "Testing alexander-laughlin submissions.\n\n\n";
-python3 py3/alexander-laughlin/00/day-zero.py
+printf "Testing xander-harris submissions.\n\n\n";
+python3 py3/xander-harris/00/day-zero.py
 printf "\nTest zero passed.\n\n";
-python3 py3/alexander-laughlin/01/day-one.py
+python3 py3/xander-harris/01/day-one.py
 printf "\nTest one passed. \n\n";
-python3 py3/alexander-laughlin/02/day-two.py
+python3 py3/xander-harris/02/day-two.py
 printf "\nTest two passed. \n\n";
-python3 py3/alexander-laughlin/03/day-three.py
+python3 py3/xander-harris/03/day-three.py
 printf "\nTest three passed. \n\n";
-python3 py3/alexander-laughlin/04/day-four.py
+python3 py3/xander-harris/04/day-four.py
 printf "\nTest four passed. \n\n";
-python3 py3/alexander-laughlin/05/day-five.py
+python3 py3/xander-harris/05/day-five.py
 printf "\nTest five passed. \n\n";
-python3 py3/alexander-laughlin/06/day-six.py
+python3 py3/xander-harris/06/day-six.py
 printf "\nTest six passed. \n\n";
-python3 py3/alexander-laughlin/07/day-seven.py
+python3 py3/xander-harris/07/day-seven.py
 printf "\nTest seven passed. \n\n";
-python3 py3/alexander-laughlin/08/day-eight.py
+python3 py3/xander-harris/08/day-eight.py
 printf "\nTest eight passed. \n\n";
-python3 py3/alexander-laughlin/09/day-nine.py
+python3 py3/xander-harris/09/day-nine.py
 printf "\nTest nine passed. \n\n";
 
-printf "Testing jessica-laughlin submissions.\n\n\n";
-python3 py3/jessica-laughlin/00/day-zero.py
+printf "Testing jessica-hartwell submissions.\n\n\n";
+python3 py3/jessica-hartwell/00/day-zero.py
 printf "\nTest zero passed.\n\n";
-python3 py3/jessica-laughlin/01/day-one.py
+python3 py3/jessica-hartwell/01/day-one.py
 printf "\nTest one passed. \n\n";
-python3 py3/jessica-laughlin/02/day-two.py
+python3 py3/jessica-hartwell/02/day-two.py
 printf "\nTest two passed. \n\n";
-python3 py3/jessica-laughlin/03/day-three.py
+python3 py3/jessica-hartwell/03/day-three.py
 printf "\nTest three passed. \n\n";
-python3 py3/jessica-laughlin/04/day-four.py
+python3 py3/jessica-hartwell/04/day-four.py
 printf "\nTest four passed. \n\n";
-python3 py3/jessica-laughlin/05/day-five.py
+python3 py3/jessica-hartwell/05/day-five.py
 printf "\nTest five passed. \n\n";
-python3 py3/jessica-laughlin/06/day-six.py
+python3 py3/jessica-hartwell/06/day-six.py
 printf "\nTest six passed. \n\n";
-python3 py3/jessica-laughlin/07/day-seven.py
+python3 py3/jessica-hartwell/07/day-seven.py
 printf "\nTest seven passed. \n\n";
-python3 py3/jessica-laughlin/08/day-eight.py
+python3 py3/jessica-hartwell/08/day-eight.py
 printf "\nTest eight passed. \n\n";
-python3 py3/jessica-laughlin/09/day-nine.py
+python3 py3/jessica-hartwell/09/day-nine.py
 printf "\nTest nine passed. \n\n";
 
