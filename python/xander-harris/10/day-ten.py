8b76391948fecab4591900725cb4edd40c6a46c0
Xander Harris <duchess@gahan-corporation.com>
2018-02-28 09:02:47 -0800

    If only I were smart enough to have thought of this. See #60.

--- python/xander-harris/10/day-ten.py
+++ python/xander-harris/10/day-ten.py
@@ -1,32 +1,18 @@
 #!/usr/bin/env python3
 """HackerRank Day 10."""
+n = int(input().strip())
 
 
 def decimal_to_binary(quotient, binary):
     """Convert decimal to binary."""
-    print(quotient)
     new_quotient = quotient // 2
     remainder = quotient % 2
     binary.append(str(int(remainder)))
     if new_quotient > 0.9:
         return decimal_to_binary(new_quotient, binary)
     else:
-        binary = ''.join(binary[::-1])
+        binary = len(max(''.join(binary[::-1]).split("0"), key=len))
         return binary
 
 
-def count_consecutive_ones(binary):
-    """Count consecutive ones."""
-    print(binary)
-    count = 1
-
-    for i in range(1, len(binary)):
-        if binary[i] == 0:
-            continue
-
-        if binary[i-1] == binary[i]:
-            count = count + 1
-    print(count)
-
-
-[count_consecutive_ones(decimal_to_binary(i, [])) for i in range(1, 512)]
+print(decimal_to_binary(n, []))
